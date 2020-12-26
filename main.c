dd234ebac3cc2d89283a78ee2cce4cf63adb6253
alt-romes <rodrigo.m.mesquita@gmail.com>
2020-12-26 20:42:52 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct registers {
+    char a;
+    char b;
+    char c;
+    char d;
+    char e;
+    char f;
+}
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
